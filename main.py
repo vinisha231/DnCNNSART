86285c8a58e1de4f23a03ccc65ee397bbb09390e
Vinisha <129325340+vinisha231@users.noreply.github.com>
2024-07-26 15:25:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,26 @@
-# Import required libraries and modules
-import os
-import struct
-import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.keras import layers, models, Input
-from tensorflow.keras.layers import Subtract
-from glob import glob
-import astra
 import argparse
-from bm3d import bm3d
+from glob import glob
+import os
+from PIL import Image
+import numpy as np
+import astra
+from tensorflow.keras.models import load_model, Sequential
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation
 
-# DnCNN model definition
-def DnCNN(depth=17, filters=64, image_channels=1, use_bn=True):
-    input_layer = Input(shape=(None, None, image_channels), name='input')
-    x = layers.Conv2D(filters, kernel_size=3, padding='same', activation='relu')(input_layer)
-    for _ in range(depth-2):
-        x = layers.Conv2D(filters=filters, kernel_size=3, padding='same')(x)
-        if use_bn:
-            x = layers.BatchNormalization()(x)
-        x = layers.Activation('relu')(x)
-    output_layer = layers.Conv2D(filters=image_channels, kernel_size=3, padding='same')(x)
-    output_layer = Subtract()([input_layer, output_layer])
-    model = models.Model(inputs=input_layer, outputs=output_layer, name='DnCNN')
+# Function to define the DnCNN model architecture
+def create_dncnn_model():
+    model = Sequential()
+    model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same', input_shape=(None, None, 1)))
+    model.add(Activation('relu'))
+    for _ in range(15):
+        model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same'))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+    model.add(Conv2D(filters=1, kernel_size=(3, 3), padding='same'))
+    model.compile(optimizer='adam', loss='mean_squared_error')
     return model
 
-# Function to read .flt file
-def read_flt_file(file_path, shape):
-    with open(file_path, 'rb') as f:
-        data = f.read()
-        return np.array(struct.unpack('f' * (len(data) // 4), data)).reshape(shape)
-
-# Function to save .flt file
-def save_flt_file(file_path, data):
-    with open(file_path, 'wb') as f:
-        f.write(struct.pack('f' * data.size, *data.flatten()))
-
-# Function to display images
-def display_images(original, noisy, denoised):
-    plt.figure(figsize=(15, 5))
-    plt.subplot(1, 3, 1)
-    plt.title('Original Image')
-    plt.imshow(original, cmap='gray')
-    plt.axis('off')
-    plt.subplot(1, 3, 2)
-    plt.title('Noisy Image')
-    plt.imshow(noisy, cmap='gray')
-    plt.axis('off')
-    plt.subplot(1, 3, 3)
-    plt.title('Denoised Image')
-    plt.imshow(denoised, cmap='gray')
-    plt.axis('off')
-    plt.show()
-
-# Function to create projectors
+# Function Definitions
 def create_projector(geom, numbin, angles, dso, dod, fan_angle):
     if geom == 'parallel':
         proj_geom = astra.create_proj_geom(geom, 1.0, numbin, angles)
@@ -65,127 +32,206 @@ def create_projector(geom, numbin, angles, dso, dod, fan_angle):
     p = astra.create_projector('cuda', proj_geom, vol_geom)
     return p
 
-# Argument parser function
 def generateParsedArgs():
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--sino', dest='infile', default='.', help='input sinogram -- directory or single file')
-    parser.add_argument('--out', dest='outfolder', default='.', help='output directory')
-    parser.add_argument('--numpix', dest='numpix', type=int, default=512, help='size of volume (n x n)')
-    parser.add_argument('--psize', dest='psize', default='', help='pixel size (float) OR file containing pixel sizes (string)')
-    parser.add_argument('--numbin', dest='numbin', type=int, default=729, help='number of detector pixels')
-    parser.add_argument('--ntheta', dest='numtheta', type=int, default=900, help='number of angles')
-    parser.add_argument('--nsubs', dest='ns', type=int, default=1, help='number of subsets. must divide evenly into number of angles')
-    parser.add_argument('--range', dest='theta_range', type=float, nargs=2, default=[0, 360], help='starting and ending angles (deg)')
-    parser.add_argument('--geom', dest='geom', default='fanflat', help='geometry (parallel or fanflat)')
-    parser.add_argument('--dso', dest='dso', type=float, default=100, help='source-object distance (cm) (fanbeam only)')
-    parser.add_argument('--dod', dest='dod', type=float, default=100, help='detector-object distance (cm) (fanbeam only)')
-    parser.add_argument('--fan_angle', dest='fan_angle', default=35, type=float, help='fan angle (deg) (fanbeam only)')
-    parser.add_argument('--numits', dest='num_its', default=32, type=int, help='maximum number of iterations')
-    parser.add_argument('--beta', dest='beta', default=1., type=float, help='relaxation parameter beta')
-    parser.add_argument('--x0', dest='x0_file', default='', help='initial image (default: zeros)')
-    parser.add_argument('--xtrue', dest='xtrue_file', default='', help='true image (if available)')
-    parser.add_argument('--sup_params', dest='sup_params', type=float, nargs=4, help='superiorization parameters: k_min, k_step, gamma, bm3d_sigma')
-    parser.add_argument('--epsilon_target', dest='epsilon_target', default=0., help='target residual value (float, or file with residual values)')
-    parser.add_argument('--ckpt_dir', dest='ckpt_dir', default='./checkpoint', help='directory containing checkpoint for DnCnn')
-    parser.add_argument('--make_png', dest='make_png', type=bool, default=False, help='whether or not you would like to generate .png files')
-    parser.add_argument('--make_intermediate', dest='make_intermediate', type=bool, default=False, help='whether or not you would like to generate output files each iter')
-    parser.add_argument('--overwrite', dest='overwrite', type=bool, default=True, help='whether you would like to reprocess preexisting files on export')
+    parser.add_argument('--sino',dest='infile', required=False, default='/mmfs1/gscratch/uwb/CT_images/sinos2024/60views', help='Path to sinogram files')
+    #parser.add_argument('--infile', type=str, required=False, default='/mmfs1/gscratch/uwb/CT_images/RECONS2024/900views', help='Path to the input file or directory')
+    parser.add_argument('--out', required=False, default='/mmfs1/gscratch/uwb/vdhaya/SARTDnCNN/output', help='Output directory')
+    parser.add_argument('--psize', required=False, default=0.0568, help='Pixel size or path to pixel size file')
+    parser.add_argument('--numits', required=False, type=int, default=1000, help='Number of iterations')
+    parser.add_argument('--beta', required=False, type=float, default=1.9, help='Regularization parameter')
+    parser.add_argument('--epsilon_target', required=False, default='/mmfs1/gscratch/uwb/CT_images/recons2024/60views/residuals.txt', help='Target residual or path to target residual file')
+    parser.add_argument('--x0', required=False, default=None, help='Path to initial images')
+    parser.add_argument('--xtrue', required=False, default='/mmfs1/gscratch/uwb/CT_images/recons2024/900views/', help='Path to true images')
+    parser.add_argument('--sup_params', required=False, nargs=3, type=float, default=[10, 1, 0.95], help='Superparameters for DnCNN')
+    parser.add_argument('--make_png', required=False, choices=['True', 'False'], default='False', help='Whether to save as PNG')
+    parser.add_argument('--overwrite', required=False, choices=['True', 'False'], default='False', help='Whether to overwrite existing files')
+    parser.add_argument('--make_intermediate', required=False, choices=['True', 'False'], default='False', help='Whether to save intermediate files')
+    parser.add_argument('--model_weights', required=False, default='/mmfs1/gscratch/uwb/vdhaya/dncnn_model.weights.h5', help='Path to the trained model weights file')
+    parser.add_argument('--geom', required=False, default='fanflat', help='Geometry type (e.g., fanflat, parallel)')
+    parser.add_argument('--dso', required=False, type=float, default=100, help='Distance source to object')
+    parser.add_argument('--dod', required=False, type=float, default=100, help='Distance object to detector')
+    parser.add_argument('--fan_angle', required=False, type=float, default=35.0, help='Fan angle')
+    parser.add_argument('--numpix', required=False, type=int, default=512, help='Number of pixels')
+    parser.add_argument('--numbin', required=False, type=int, default=729, help='Number of bins')
+    parser.add_argument('--numtheta', required=False, type=int, default=60, help='Number of angles')
+    parser.add_argument('--theta_range', required=False, nargs=2, type=float, default=[0, 360], help='Range of angles')
+    parser.add_argument('--ns', required=False, type=int, default=12, help='Number of subsets')
     return parser.parse_args()
 
-# Main function
-if __name__ == "__main__":
-    # Parse Arguments & Initialize
-    args = generateParsedArgs()
+def makePNG(f, outname):
+    img = np.maximum(f, np.finfo(float).eps)
+    img = (img.T / np.amax(f)) * 255
+    img = np.round(img)
+    img = Image.fromarray(img.astype('uint8')).convert('L')
+    img.save(outname + '.png', 'png')
+    return
 
-    infile = args.infile
-    outfolder = args.outfolder
-    x0file = args.x0_file
-    xtruefile = args.xtrue_file
-    psize = args.psize
-    numpix = args.numpix
-    numbin = args.numbin
-    numtheta = args.numtheta
-    ns = args.ns
-    numits = args.num_its
-    beta = args.beta
-    epsilon_target = args.epsilon_target
-    theta_range = args.theta_range
-    geom = args.geom
-    dso = args.dso
-    dod = args.dod
-    fan_angle = args.fan_angle
-    make_png = bool(args.make_png)
-    overwrite = bool(args.overwrite)
-    make_intermediate = bool(args.make_intermediate)
+def makeFLT(f, outname):
+    img = np.float32(f)
+    img = np.maximum(img, np.finfo(np.float32).eps)
+    img.tofile(outname + '.flt')
+    return
 
-    # Superiorization parameters
-    use_sup = False
-    kmin, kstep, gamma, sigma, alpha = 10, 1, 0, 0, 1  # Set kmin to 10 and kstep to 1
-    if args.sup_params:
-        use_sup = True
-        kmin, kstep, gamma, sigma = map(float, args.sup_params)
+# Main
+args = generateParsedArgs()
+infile = args.infile
+outfolder = args.out
+x0file = args.x0
+psize = args.psize
+numpix = args.numpix
+numbin = args.numbin
+numtheta = args.numtheta
+ns = args.ns
+numits = args.numits
+beta = args.beta
+epsilon_target = args.epsilon_target
+theta_range = args.theta_range
+geom = args.geom
+dso = args.dso
+dod = args.dod
+fan_angle = args.fan_angle
+make_png = bool(args.make_png)
+overwrite = bool(args.overwrite)
+make_intermediate = bool(args.make_intermediate)
+use_sup = False
+kmin = 0    #Iteration at which superiorization begins
+kstep = 0   #Interval of SARTS between each superiorization step
+gamma = 0   #Geometric attenuation factor for superiorization
+alpha = 1   #Computed attenuation factor for superiorization, not an arg
+if not (args.sup_params is None):
+    use_sup = True
+    kmin = int(args.sup_params[0])
+    kstep = int(args.sup_params[1])
+    gamma = args.sup_params[2]
+# Load DnCNN model
+model = create_dncnn_model()
+model.load_weights('/mmfs1/gscratch/uwb/vdhaya/dncnn_model.weights.h5')
 
-    eps = np.finfo(float).eps
+eps = np.finfo(float).eps
+#fnames = sorted(glob(x0file + '/*.flt')) if os.path.isdir(x0file) else [x0file]
+fnames = []
+if os.path.isdir(infile):
+    fnames = sorted(glob(infile + '/*.flt'))
+#Otherwise, a single filename was provided
+else:
+    fnames.append(infile)
+#psizes = float(psize) if not psize.endswith('.txt') else np.loadtxt(psize, dtype='f')
+# Check if psize is a path or a number
+if isinstance(psize, str):
+    if psize.endswith('.txt'):
+        psizes = np.loadtxt(psize, dtype='f')
+    else:
+        psizes = float(psize)
+else:
+    psizes = float(psize)
+#epsilon_target = float(epsilon_target) if not epsilon_target.endswith('.txt') else np.loadtxt(epsilon_target, dtype='f')
+# Check if epsilon_target is a path or a number
+if isinstance(epsilon_target, str):
+    if epsilon_target.endswith('.txt'):
+        epsilon_target = np.loadtxt(epsilon_target, dtype='f')
+    else:
+        epsilon_target = float(epsilon_target)
+else:
+    epsilon_target = float(epsilon_target)
+vol_geom = astra.create_vol_geom(numpix, numpix)
+theta_range = np.deg2rad(theta_range)
+angles = theta_range[0] + np.linspace(0, numtheta - 1, numtheta, False) * (theta_range[1] - theta_range[0]) / numtheta
 
-    fnames = sorted(glob(infile + '/*.flt')) if os.path.isdir(infile) else [infile]
+P, Dinv, D_id, Minv, M_id = [None] * ns, [None] * ns, [None] * ns, [None] * ns, [None] * ns
+for j in range(ns):
+    ind1 = range(j, numtheta, ns)
+    p = create_projector(geom, numbin, angles[ind1], dso, dod, fan_angle)
+    D_id[j], Dinv[j] = astra.create_backprojection(np.ones((numtheta // ns, numbin)), p)
+    M_id[j], Minv[j] = astra.create_sino(np.ones((numpix, numpix)), p)
+    Dinv[j] = np.maximum(Dinv[j], eps)
+    Minv[j] = np.maximum(Minv[j], eps)
+    P[j] = p
+print(f"Attempting to open file: {outfolder + '/residuals.txt'}")
+res_file = open(outfolder + "/residuals.txt", "w+")
+for n in range(len(fnames)):
+    name = fnames[n]
+    head, tail = os.path.split(name)
+    head, tail = tail.split("_", 1)
+    outname = outfolder + "/" + head + "_recon_"
+    print("\nReconstructing " + head + ":")
 
-    psizes = float(psize) if not psize.endswith('.txt') else np.loadtxt(psize, dtype='f')
+    sino = np.fromfile(name, dtype='f').reshape(numtheta, numbin)
+    f = np.zeros((numpix, numpix))
 
-    epsilon_target = float(epsilon_target) if not epsilon_target.endswith('.txt') else np.loadtxt(epsilon_target, dtype='f')
+    dx = psizes[n] if isinstance(psizes, np.ndarray) else psizes
+    etarget = epsilon_target[n] if isinstance(epsilon_target, np.ndarray) else epsilon_target
 
-    vol_geom = astra.create_vol_geom(numpix, numpix)
-    theta_range = np.deg2rad(theta_range)
-    angles = theta_range[0] + np.linspace(0, numtheta-1, numtheta, False) * (theta_range[1]-theta_range[0]) / numtheta
+    for k in range(1, numits + 1):
+        if (not overwrite) and os.path.exists(outname + str(k) + '_SART.flt'):
+            break
 
-    calc_error = False
+        if (use_sup) and (k >= kmin) and ((k - kmin) % kstep == 0):
+            print("Applying DnCNN before the next SART iteration...")
+            f_out = model.predict(np.expand_dims(f, axis=(0, -1)))[0, :, :, 0]
+            p = f_out - f
+            pnorm = np.linalg.norm(p, 'fro') + eps
+            print("pnorm: " + str(pnorm))
+            if k == kmin:
+                alpha = pnorm
+            else:
+                alpha *= gamma
+            print("alpha: " + str(alpha) + '\n')
+            if pnorm > alpha:
+                p = alpha * p / (np.linalg.norm(p, 'fro') + eps)
+                f = f + p
+            else:
+                f = f_out
+            if make_intermediate:
+                makeFLT(f, outname + str(k) + '_dncnn_sup')
+                if make_png:
+                    makePNG(f, outname + str(k) + '_dncnn_sup')
 
-    P, Dinv, D_id, Minv, M_id = [None]*ns, [None]*ns, [None]*ns, [None]*ns, [None]*ns
-    for j in range(ns):
-        ind1 = range(j, numtheta, ns)
-        p = create_projector(geom, numbin, angles[ind1], dso, dod, fan_angle)
-        D_id[j], Dinv[j] = astra.create_backprojection(np.ones((numtheta//ns, numbin)), p)
-        M_id[j], Minv[j] = astra.create_sino(np.ones((numpix, numpix)), p)
-        Dinv[j] = np.maximum(Dinv[j], eps)
-        Minv[j] = np.maximum(Minv[j], eps)
-        P[j] = p
-
-    for i in range(len(fnames)):
-        sino = read_flt_file(fnames[i], (numtheta, numbin))
-
-        x0 = read_flt_file(x0file, (numpix, numpix)) if x0file else np.zeros((numpix,numpix), dtype=‘f’)
-        xtrue = read_flt_file(xtruefile, (numpix, numpix)) if xtruefile else np.zeros((numpix, numpix), dtype=‘f’)
-        x = x0
-
-    for k in range(numits):
         for j in range(ns):
             ind1 = range(j, numtheta, ns)
-            sino_j = sino[ind1, :]
-            sino_j += np.random.normal(scale=psizes, size=sino_j.shape)
-            proj_id, projdata = astra.create_sino(x, P[j])
-            astra.data2d.store(proj_id, sino_j)
-            bp_id, bpdata = astra.create_backprojection(projdata, P[j])
-            astra.data2d.store(bp_id, bpdata)
+            p = P[j]
+            fp_id, fp = astra.create_sino(f, p)
+            diffs = (sino[ind1, :] - fp * dx) / Minv[j] / dx
+            bp_id, bp = astra.create_backprojection(diffs, p)
+            ind2 = np.abs(bp) > 1e3
+            bp[ind2] = 0
+            f = f + beta * bp / Dinv[j]
+            astra.data2d.delete(fp_id)
+            astra.data2d.delete(bp_id)
+        f=np.maximum(f,eps)
+        if make_intermediate:
+            makeFLT(f, outname + str(k) + '_SART')
+            if make_png:
+                makePNG(f, outname + str(k) + '_SART')
 
-            epsilon = np.zeros(numits)
-            for _ in range(numits):
-                _, res = astra.create_sino(x, P[j])
-                epsilon[_] = np.linalg.norm(res)
-            x = x + beta * np.mean(epsilon)
+        fp = np.zeros((numtheta, numbin))
+        for j in range(ns):
+            ind = range(j, numtheta, ns)
+            p = P[j]
+            fp_tempid, fp_temp = astra.create_sino(f, p)
+            fp[ind, :] = fp_temp * dx
+            astra.data2d.delete(fp_tempid)
+        res = np.linalg.norm(fp - sino, 'fro')
+        print('Iteration #{0:d}: Residual = {1:1.4f}\n'.format(k, res))
+        if res < etarget:
+            print("Target residual for " + head + " of ", end='')
+            print(str(etarget) + " reached!")
+            break
 
-    # Load pre-trained DnCNN model for denoising
-    dncnn_model = DnCNN()
-    dncnn_model.load_weights(args.ckpt_dir)
+    res_file.write("%f\n" % res)
+    if use_sup:
+        makeFLT(f, outname + str(k) + '_DnCNNsup')
+        if make_png:
+            makePNG(f, outname + str(k) + '_DnCNNsup')
+    else:
+        makeFLT(f, outname + str(k) + '_SART')
+        if make_png:
+            makePNG(f, outname + str(k) + '_SART')
 
-    # Use DnCNN for denoising
-    noisy_img = read_flt_file(fnames[i], (numpix, numpix))
-    noisy_img_reshaped = np.reshape(noisy_img, (1, numpix, numpix, 1))
-    denoised_img = dncnn_model.predict(noisy_img_reshaped)
-    denoised_img = np.reshape(denoised_img, (numpix, numpix))
-
-    denoised_img = denoised_img.astype(np.float32)
-    save_flt_file(os.path.join(outfolder, os.path.basename(fnames[i])), denoised_img)
-
-    # Display the images
-    display_images(xtrue, noisy_img, denoised_img)
-
-    print(f'Successfully processed: {fnames[i]}')
+print("\n\nExiting...")
+for j in range(ns):
+    astra.data2d.delete(D_id[j])
+    astra.data2d.delete(M_id[j])
+    astra.projector.delete(P[j])
+res_file.close()
+                                                                    
